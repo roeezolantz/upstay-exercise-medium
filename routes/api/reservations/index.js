97381256edd56711b0f638078aaeae0c5e1062a4
Roee Zolantz <xdlunatic@gmail.com>
2020-01-03 04:02:03 +0200

    Removed console.logs

--- routes/api/reservations/index.js
+++ routes/api/reservations/index.js
@@ -4,12 +4,10 @@ import { getAllReservations } from '../../../db/reservations'
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    console.log("Fetching reservations from the db...");
     let result;
 
     try {
         result = await getAllReservations()
-        console.log("getAllReservations returned : ", result)
         res.json(result);
     } catch (error) {
         res.sendStatus(500).send(error)
