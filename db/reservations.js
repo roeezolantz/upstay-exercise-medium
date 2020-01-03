bfa18b7fe3451fa67da6a04abba6c866d4029a28
Roee Zolantz <xdlunatic@gmail.com>
2020-01-03 03:55:59 +0200

    Fixed results destructuring

--- db/reservations.js
+++ db/reservations.js
@@ -4,10 +4,9 @@ export const addReservation = async reservation => {
     const TABLE_NAME = `public.reservations`
     const INSERT_RESERVATIONS = `INSERT INTO ${TABLE_NAME} (uuid, hotel_id, currency, price, guest_name, room_name, arrival_date, nights) VALUES ($1,$2,$3,$4,$5,$6,$7,$8);`
 
-    let response;
     try {
-        response = await query(INSERT_RESERVATIONS, Object.values(reservation));
-        return response.rowCount == 1;
+        let { rowCount } = await query(INSERT_RESERVATIONS, Object.values(reservation));
+        return rowCount == 1;
     } catch (error) {
         console.log(error);
     }
@@ -15,12 +14,11 @@ export const addReservation = async reservation => {
 
 export const getAllReservations = async () => {
     const TABLE_NAME = `public.reservations`
-    const GET_ALL_RESERVATIONS = `SELECT * FROM ${TABLE_NAME};`
-    let results;
+    const GET_ALL_RESERVATIONS = `select * from reservations`
+
     try {
-        results = await query(GET_ALL_RESERVATIONS).rows;
-        console.log(results)
-        return results;
+        const { rows } = await query(GET_ALL_RESERVATIONS)
+        return rows;
     } catch (error) {
         console.log(error);
     }
