41bae24d71510c5f86c0c68ad645bd0317409005
Roee Zolantz <xdlunatic@gmail.com>
2020-01-03 04:01:30 +0200

    Added API rest endpoint to get all the reservations from the db

--- routes/api/reservations/index.js
+++ routes/api/reservations/index.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import { getAllReservations } from '../../../db/reservations'
 
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+    console.log("Fetching reservations from the db...");
+    let result;
+
+    try {
+        result = await getAllReservations()
+        console.log("getAllReservations returned : ", result)
+        res.json(result);
+    } catch (error) {
+        res.sendStatus(500).send(error)
+    }
+});
 
 export default router;
